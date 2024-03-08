1f5b1a43a28d0be852211e34c386f368c07210a1
zachwetsel <zachwetsel@yahoo.com>
2024-03-07 21:30:56 -0700

    Finished Book of Mormon list js

---
+++ week02/scripts/bom.js
@@ -0,0 +1,19 @@
+
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+button.addEventListener('click', () => {if (InputDeviceInfo.value != '') 
+{const li = document.createElement('li');
+const deleteButton = document.createElement('button');
+li.textContent = input.value;
+deleteButton.textContent = '❌';
+li.append(deleteButton);
+list.append(li);
+deleteButton.addEventListener('click', function () {
+    list.removeChild(li);
+    input.focus();
+});
+input.focus();
+input.value = '';
+}});
