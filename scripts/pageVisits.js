42f729c4e22659f03a5e1eb4144ebe40d97c2232
zachwetsel <zachwetsel@yahoo.com>
2024-03-20 15:49:04 -0600

    Fixed other bug with pageVisits

--- scripts/pageVisits.js
+++ scripts/pageVisits.js
@@ -1,6 +1,6 @@
 const visitsDisplay = document.querySelector(".visits");
 
-let numVisits = Number(window.localStorage.getItem("numVisits.ls")) || 0;
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
 
 if (numVisits !== 0) {
     visitsDisplay.textContent = numVisits;
