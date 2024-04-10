155f730c1931edf096c65c1f3aa707352217a31f
zachwetsel <zachwetsel@yahoo.com>
2024-04-10 15:33:39 -0600

    Updated Home Page with JSON links

--- scripts/links.js
+++ scripts/links.js
@@ -4,6 +4,34 @@ const linksURL = "https://zachwetsel.github.io/wdd230/data/links.json";
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    console.log(data);
+    displayLinks(data[0].weeks);
 }
 getLinks();
+
+function displayLinks(weeks) {
+    const activitiesList= document.getElementById("activities-list");
+    activitiesList.innerHTML = "";
+
+    weeks.forEach((week) => {
+        const weekNumber = week.week;
+        const weekLinks = week.links;
+
+        const weekListItem = document.createElement("li");
+        const weekTitle = document.createElement("h3");
+        weekTitle.textContent = weekNumber;
+        weekListItem.appendChild(weekTitle);
+
+        const linksList = document.createElement("ul");
+        weekLinks.forEach(link => {
+            const listItem = document.createElement("li");
+            const anchor = document.createElement("a");
+            anchor.textContent = link.title;
+            anchor.href = `${baseURL}${link.url}`;
+            listItem.appendChild(anchor);
+            linksList.appendChild(listItem);
+        });
+
+        weekListItem.appendChild(linksList);
+        activitiesList.appendChild(weekListItem);
+    });
+}
